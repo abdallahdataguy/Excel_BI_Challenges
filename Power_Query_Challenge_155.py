ef0026ce4cc86cbc058c2a0f32cc0f275951e857
Abdallah Ally <31002385+mathematiciantz@users.noreply.github.com>
2024-02-10 18:37:09 +0300

    Power_Query_Challenge_155
    Python solution to Excel BI (Vijay Verma) Power_Query_Challenge_155

---
+++ Power_Query_Challenge_155.py
@@ -0,0 +1,23 @@
+# Link to the challenge: https://www.linkedin.com/feed/update/urn:li:activity:7161931798501867520/
+
+import pandas as pd
+import re
+
+# Read the file
+file_path = 'Power_Query_Challenge_155.xlsx'
+df = pd.read_excel(file_path, usecols=[0, 3])
+
+# Create a function to extract valid times from a string
+def extract_valid_times(col):
+    valid_times = [x for x in re.findall(r'\d{1,2}:\d{2}', col) 
+                     if int(x.split(':')[0]) < 24 and int(x.split(':')[1]) < 60]
+    return ', '.join(valid_times)
+
+# Create a new column containing valid times extracted from a string 
+# of values using a user defined function developed above
+df['My Answer'] = df['String'].apply(extract_valid_times)
+
+# Replace empty string values with nan values
+df['My Answer'] = df['My Answer'].replace('', float('nan'))
+
+print(df)
