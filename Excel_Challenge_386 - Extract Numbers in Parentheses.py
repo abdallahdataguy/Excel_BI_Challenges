4bf40d6280e792001d948ac97ba0f439e731b25a
Abdallah Ally <31002385+mathematiciantz@users.noreply.github.com>
2024-02-10 18:48:25 +0300

    Create Excel_Challenge_386 - Extract Numbers in Parentheses.py
    Python solution to Excel BI (Vijay Verma) Excel_Challenge_386

---
+++ Excel_Challenge_386 - Extract Numbers in Parentheses.py
@@ -0,0 +1,20 @@
+import re
+import pandas as pd
+
+# Specify a file name
+file_name = r'C:\Users\aally\Downloads\Excel_Challenge_386 - Extract Numbers in Parentheses.xlsx'
+
+# Read the file
+df = pd.read_excel(file_name)
+
+# Create a function to extract the numbers in parentheses given a string
+def answer(col):
+    return ", ".join(re.findall(r'\((\d+)\)', col))
+    
+# Add a new colum 'My Answer' to the dataframe using the function above
+df['My Answer'] = df['String'].apply(answer)
+
+# Replace empty string values by na values
+df['My Answer'] = df['My Answer'].replace('', float('nan'))
+
+print(df)
