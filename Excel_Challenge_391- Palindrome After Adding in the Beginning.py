bf4fe25dedd2e03cf5b375d638da5b8c0b61c279
Abdallah Ally <31002385+mathematiciantz@users.noreply.github.com>
2024-02-14 17:38:21 +0300

    Create Excel_Challenge_391- Palindrome After Adding in the Beginning.py
    Python solution to Excel BI Excel_Challenge_391- Palindrome After Adding in the Beginning

---
+++ Excel_Challenge_391- Palindrome After Adding in the Beginning.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[60]:
+
+
+import pandas as pd
+
+# Read an Excel file
+file_path = 'Excel_Challenge_391- Palindrome After Adding in the Beginning.xlsx'
+df = pd.read_excel(file_path)
+
+# Create a function to generate the required results
+def make_palindrome(col):
+    value = col
+    if value == value[::-1]:
+        return value
+    for i in range(-1, -len(col), -1):
+        test = col[i:][::-1] + value
+        if test == test[::-1]:
+            value = test
+            break
+    return value
+
+# Add new column by applying the function created above 
+df['My Answer'] = df['String'].apply(make_palindrome)
+    
+print(df)
+
+
+# In[ ]:
+
+
+
+
