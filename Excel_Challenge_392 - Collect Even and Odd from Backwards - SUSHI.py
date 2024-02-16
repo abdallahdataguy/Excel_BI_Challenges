bc8106f132c5ac70a0944e46615b221d43fc2031
Abdallah Ally <31002385+mathematiciantz@users.noreply.github.com>
2024-02-16 10:54:55 +0300

    Create Excel_Challenge_392 - Collect Even and Odd from Backwards - SUSHI.py
    Alternative Python solution to Excel BI challenge Excel_Challenge_392 - Collect Even and Odd from Backwards

---
+++ Excel_Challenge_392 - Collect Even and Odd from Backwards - SUSHI.py
@@ -0,0 +1,20 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7163743730468954112-mGb-/
+
+import pandas as pd
+
+# Read the Excel file
+file_path = 'Excel_Challenge_392 - Collect Even and Odd from Backwards.xlsx'
+df = pd.read_excel(file_path, usecols='a:b')
+
+# Create a function to generate the required output
+def collect_letters(col):
+    values = list(reversed(col))
+    even, odd = values[1::2], values[::2] # unpacking
+    return "".join(even + odd)
+
+# Add new column using the above function
+# and print the results
+df['My Answer'] = df['String'].apply(collect_letters)
+
+print(df)
