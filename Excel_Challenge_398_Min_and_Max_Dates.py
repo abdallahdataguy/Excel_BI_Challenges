af0f8d733b00b10c55666d10246a493fd2941238
Abdallah Ally <31002385+mathematiciantz@users.noreply.github.com>
2024-02-24 00:12:02 +0300

    Create Excel_Challenge_398_Min_and_Max_Dates.py
    My Python solution to Excel BI challenge "Excel_Challenge_398 - Min and Max Dates"

---
+++ Excel_Challenge_398_Min_and_Max_Dates.py
@@ -0,0 +1,28 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7166642838963699712-Ja5K/
+
+import pandas as pd
+
+# read the Excel file
+file_path = 'Excel_Challenge_398 - Min and Max Dates.xlsx'
+df1 = pd.read_excel(file_path, header=1, usecols='C:F').dropna()
+df1[['Year', 'Month']] = df1[['Year', 'Month']].astype(int)
+df2 = pd.read_excel(file_path, usecols='A')
+
+# Extract year and month
+df2['Year'] = df2['Date'].dt.year
+df2['Month'] = df2['Date'].dt.month
+
+# Group by year and month, and calculate min and max dates
+df2 = df2.groupby(['Year', 'Month']).agg({'Date': ['min', 'max']})
+
+# Flatten MultiIndex columns
+df2.columns = ['_'.join(col) for col in df2.columns]
+df2.rename(columns={'Date_min': 'Min Date', 'Date_max': 'Max Date'}, inplace=True)
+
+# Reset index to make 'Year' and 'Month' columns
+df2.reset_index(inplace=True)
+
+# Print the first five records
+print(f'\nAnswer Expected: \n{df1.head()}')
+print(f'\nMy Answer: \n{df2.head()}')
