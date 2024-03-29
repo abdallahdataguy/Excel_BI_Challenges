b8df7d3742518e3601eea9a358ddd5207ae8e163
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-03-29 19:26:52 +0300

    Update Excel_Challenge_423_Split_Case_Sensitive_Alphabets_and_Numbers.py

--- Excel_Challenge_423_Split_Case_Sensitive_Alphabets_and_Numbers.py
+++ Excel_Challenge_423_Split_Case_Sensitive_Alphabets_and_Numbers.py
@@ -3,9 +3,11 @@
 
 import pandas as pd
 
+# Read the Excel file
 file_path = 'Excel_Challenge_423 - Split Case Sensitive Alphabets and Numbers.xlsx'
 df = pd.read_excel(file_path)
 
+# Create functions to generate the desired output
 def case(char):
     if ord(char) in range(97, 123): return 'lower'
     if ord(char) in range(65, 91): return 'upper'
@@ -19,6 +21,8 @@ def split_case(text):
         else:
             chars += text[i] + ', '
     return chars + text[-1]
+pd.set_option('display.max_columns', 5)
 
+# Add the results column and print the uotput
 df['My Answer'] = df['Data'].apply(split_case)
-print(df)
+print(f'Expected Results:\n{df.iloc[:, [0, 1]]}\n\nMy Results:\n{df.iloc[:, [0, 2]]}')
