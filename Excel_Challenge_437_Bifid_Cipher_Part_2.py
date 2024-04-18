138df6e4cee7e51ae0c11c24a0bd60d39a024da5
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-04-18 17:00:01 +0300

    Update Excel_Challenge_437_Bifid_Cipher_Part_2.py

--- Excel_Challenge_437_Bifid_Cipher_Part_2.py
+++ Excel_Challenge_437_Bifid_Cipher_Part_2.py
@@ -8,8 +8,8 @@ from math import ceil
 # Create a function to generate the required results
 def bifid_cipher2(col1, col2):
     characters = ''
-    string = col2.replace('j', 'i') + ascii_lowercase.replace('j', '')
-    for char in string:
+    text = col2.replace('j', 'i') + ascii_lowercase.replace('j', '')
+    for char in text:
         if char not in characters:
             characters += char
     values = []
@@ -17,16 +17,16 @@ def bifid_cipher2(col1, col2):
         i = ceil(index / 5)
         j = index - (index // 5) * 5
         values.append((char, str(i), str(5 if j == 0 else j)))
-    string1 = [x[1] for char in col1.replace('j', 'i') for x in values if char == x[0]]
-    string2 = [x[2] for char in col1.replace('j', 'i') for x in values if char == x[0]]
-    string = ''.join(string1 + string2)
-    encrypted_string = ''
-    for i in range(0, len(string), 2):
-        sample = string[i: i + 2]
+    text1 = [x[1] for char in col1.replace('j', 'i') for x in values if char == x[0]]
+    text2 = [x[2] for char in col1.replace('j', 'i') for x in values if char == x[0]]
+    text = ''.join(text1 + text2)
+    encrypted_text = ''
+    for i in range(0, len(text), 2):
+        sample = text[i: i + 2]
         for value in values:
             if sample[0] == value[1] and sample[1] == value[2]:
-                encrypted_string += value[0]
-    return encrypted_string
+                encrypted_text += value[0]
+    return encrypted_text
 
 # Read the Excel file, perform data transformation and cleansing
 file_path = 'Excel_Challenge_437 - Bifid Cipher_Part 2.xlsx'
