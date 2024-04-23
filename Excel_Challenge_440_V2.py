ebf4aa0f4e753cf991c7a3ee3aff3e9360058035
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-04-23 08:50:41 +0300

    Create Excel_Challenge_440_V2.py

---
+++ Excel_Challenge_440_V2.py
@@ -0,0 +1,22 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7188386131313385472-ehnR/
+
+import pandas as pd
+
+# Read the Excel file
+file_path = 'Excel_Challenge_440 - List of Numbers Expressed as Sum of Two Squares.xlsx'
+df = pd.read_excel(file_path)
+
+def sum_of_two_squares(start, end):
+    values =  []
+    for i in range(1, end + 1):
+        for j in [n for n in range(1, end + 1) if n != i]:
+            total = i ** 2 + j ** 2
+            if start <= total <= end:
+                values.append(total)
+    return sorted(list(set(values)))
+
+df['My Answer'] = pd.Series(sum_of_two_squares(5, 100))
+
+# print the output
+print(df)
