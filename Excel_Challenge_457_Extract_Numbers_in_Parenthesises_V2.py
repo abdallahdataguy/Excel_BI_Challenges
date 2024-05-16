ae882117bc18d229bf387c9ca5787dd339c2c9f2
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-05-16 10:49:11 +0300

    Works with any two combination of parentheses (), [], {}, (], (} etc

---
+++ Excel_Challenge_457_Extract_Numbers_in_Parenthesises_V2.py
@@ -0,0 +1,17 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7196721087445946368-KeCD/
+
+import pandas as pd
+import re
+
+# Read the Excel file
+file_path = 'Excel_Challenge_457 - Extract Numbers in Parenthesises.xlsx'
+df = pd.read_excel(file_path).astype(str).replace('nan', '')
+
+# Perform data transformation and cleansing
+pattern = r'\[(\d+)\]|\{(\d+)\}|\((\d+)\)'
+df['My Answer'] = df['String'].apply(lambda x: ', '.join([''.join(a) for a in re.findall(pattern, x)]))
+df['Check'] = df['Answer Expected'] == df['My Answer']
+
+# Display Results
+df
