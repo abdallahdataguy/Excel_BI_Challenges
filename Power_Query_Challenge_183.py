a6114b5028c888fe65ce0e2aeeff0067b49bba99
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-05-18 17:53:10 +0300

    Update Power_Query_Challenge_183.py

--- Power_Query_Challenge_183.py
+++ Power_Query_Challenge_183.py
@@ -15,12 +15,12 @@ for i in df.index:
     v, h, r = df.iloc[i, [0, 5, 4]]
     for j in range(df.iat[i, 3]):
         y, q = df.iloc[i, [1, 2]]
-        if j + int(q[1]) <= 4:
-            items.append([v, y, 'Q' + str(j + int(q[1])), r])
+        n = j + int(q[1])
+        if n <= 4:
+            items.append([v, y, 'Q' + str(n), r])
         else:
-            y = (df.iat[i, 1] + ((j + int(q[1])) // 4 - 1 
-                                  if (j + int(q[1])) % 4 == 0 else (j + int(q[1])) // 4))
-            q = 'Q4' if (j + int(q[1])) % 4 == 0 else 'Q' + str((j + int(q[1])) % 4)
+            y = df.iat[i, 1] + (n // 4 - 1 if n % 4 == 0 else n // 4)
+            q = 'Q4' if n % 4 == 0 else 'Q' + str(n % 4)
             r = r * (1 + h / 100) if j % 4 == 0 else r
             items.append([v, y, q, r])
 
@@ -28,7 +28,7 @@ df = pd.DataFrame(items, columns=df.columns[[0, 1, 2, 4]]) # Computed
 df['Rental'] = df['Rental'].map(lambda x: round(x))
 
 # Check if my result equals the expected results
-print(f'\nMy computed vs Expected results! Are they equal? {(df.values == df1.values).all()}')
+print(f'\nMy Results vs Expected Results! Equal? {(df.values == df1.values).all()}')
 
 # Display sample results 
 df.sample(10)
