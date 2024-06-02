884bc84cc80edcfe4a1e69c27d5d71c4203a59ec
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-06-02 09:39:10 +0300

    Create Power_Query_Challenge_188.py

---
+++ Power_Query_Challenge_188.py
@@ -0,0 +1,31 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_challenge-powerquerychallenge-daxchallenge-activity-7202881638760955904-dSDC/
+
+import pandas as pd
+
+# Read the Excel file
+file_path = 'PQ_Challenge_188.xlsx'
+df = pd.read_excel(file_path, usecols='A:D', nrows=3)
+
+# Perform data wrangling
+dfs = []
+for i in df.index:
+    date_range = pd.date_range(start=df.iat[i, 1], end=df.iat[i, 2])
+    l = len(date_range)
+    values = {'Store': [df.iat[i, 0]] * l,
+              'Date': date_range,
+              'Amount': [df.iat[i, 3]] * l
+             }
+    dfs.append(pd.DataFrame(values))
+
+df = pd.concat(dfs, ignore_index=True)
+df['Quarter'] = df['Date'].map(lambda x: f"Q{x.quarter}-{x.strftime('%y')}")
+df['Order'] = (df['Quarter'] != df['Quarter'].shift(1)).cumsum()
+df['QuarterCount'] = df.groupby('Order')['Order'].transform('count')
+df['StoreCount'] = df.groupby('Store')['Store'].transform('count')
+df['NewAmount'] = df.apply(lambda x: round(x[2] * x[5] / x[6]), axis=1)
+df = df.iloc[:, [0, 3, 7]].drop_duplicates(ignore_index=True)
+df = df.rename(columns={'NewAmount': 'Amount'})
+
+# Display the final results
+df
