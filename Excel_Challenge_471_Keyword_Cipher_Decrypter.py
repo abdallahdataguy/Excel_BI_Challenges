c8035e96120a2c9b22a4cf5b9c38c16a1cd89758
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-06-05 13:08:34 +0300

    Create Excel_Challenge_471_Keyword_Cipher_Decrypter.py

---
+++ Excel_Challenge_471_Keyword_Cipher_Decrypter.py
@@ -0,0 +1,26 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7203983569814274048-0tDz/
+
+import pandas as pd
+from string import ascii_lowercase
+
+# Create a function to generate the required results
+def decrypter(text1, text2):
+    string1 = ascii_lowercase
+    string2 = ''.join(list(dict.fromkeys(text2 + ascii_lowercase)))
+    text = ''
+    for char in text1:
+        if char == ' ': text += ' '
+        else: text += string1[string2.find(char)]
+    return text
+
+# Read the Excel file
+file_path = 'Excel_Challenge_471 - Keyword Cipher Decrypter.xlsx'
+df = pd.read_excel(file_path)
+
+# Perform data wrangling
+df['My Answer'] = df.apply(lambda x: decrypter(x[0], x[1]), axis=1)
+df['Check'] = df['My Answer'] == df['Answer Expected']
+
+# Display the final results
+df
