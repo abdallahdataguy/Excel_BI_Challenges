3434f689d028a3d31bbbd9d2f158df6a6b9290b0
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-06-08 02:01:55 +0300

    Create Excel_Challenge_473_ASCII_Words.py

---
+++ Excel_Challenge_473_ASCII_Words.py
@@ -0,0 +1,29 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7204693554764406784-e5Nc/
+
+import pandas as pd
+
+# Create a function to generate the required output
+def ascii_words(word):
+    a = len(word)
+    b = '   ' + ''.join(['_   ' if char != ' ' else '  ' for char in word])[:-1] + '\n'
+    c = '  ' + ''.join(['/ \ ' if char !=' ' else '  ' for char in word]) + '\n'
+    d = ' ' + ''.join(['| ' + (char + ' ' if char != ' ' else '') for char in word]) + '|\n'
+    e = '  ' + ''.join(['\_/ ' if char !=' ' else '  ' for char in word]) 
+
+    return b + c + d + e
+
+# Read the Excel file
+file_path = 'Excel_Challenge_473 - ASCII Words.xlsx'
+df = pd.read_excel(file_path)
+
+# Perform data wrangling
+df['My Answer'] = df['Words'].map(ascii_words)
+df['Check'] = df['My Answer'] == df['Answer Expected']
+
+# Print the final results
+for word in df['Words']:
+    print(ascii_words(word))
+
+# Display the final dataset
+df
