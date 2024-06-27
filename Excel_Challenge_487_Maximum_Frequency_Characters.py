a7737879301b04125292c2759be5330f5ed57752
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-06-27 15:26:11 +0300

    Create Excel_Challenge_487_Maximum_Frequency_Characters.py

---
+++ Excel_Challenge_487_Maximum_Frequency_Characters.py
@@ -0,0 +1,23 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7211941341545598976-BVqq/
+
+import pandas as pd
+
+# Create a function to generate the required results
+def max_freq(text):
+    chars = list(text)
+    max_occur = max([chars.count(char) for char in set(chars)])
+    popular = [char for char in chars if chars.count(char) == max_occur]
+    unique = [char for i, char in enumerate(popular) 
+              if popular[: i + 1].count(char) == 1]
+    return ', '.join(unique), max_occur
+
+# Read the Excel file
+file_path = 'Excel_Challenge_487 - Maximum Frequency Characters.xlsx'
+df = pd.read_excel(file_path, skiprows=1)
+
+# Perform data wrangling
+df[['My Characters', 'My Frequency']] = df['Strings'].map(max_freq).tolist()
+
+# Display the final dataset
+df
