ed0bd2871c06bb7de2de632b12f55ad90d6c9c08
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-07-07 14:45:52 +0300

    Create Power_Query_Challenge_198.py

---
+++ Power_Query_Challenge_198.py
@@ -0,0 +1,14 @@
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_challenge-powerquerychallenge-daxchallenge-activity-7215565192485621760-otuB/
+
+# Read the data range
+df = xl("A1:B20", headers=True)
+
+# Perform data wrangling
+df['Period'] = df['Date'].map(lambda x: f'{x.year}{x.month}')
+df2 = df.groupby('Period')['Value'].max().cumsum()
+df2 = df2.reset_index().rename(columns={'Value': 'Running Total'})
+df = pd.merge(df, df2, on='Period').drop(columns='Period')
+
+# Display the final dataset
+df
