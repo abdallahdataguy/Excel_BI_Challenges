7b006fe59de082c194214b2ec966f25972335dbd
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-07-08 23:23:44 +0300

    Update Excel_Challenge_494_Tech_Numbers_V2.py

--- Excel_Challenge_494_Tech_Numbers_V2.py
+++ Excel_Challenge_494_Tech_Numbers_V2.py
@@ -10,14 +10,11 @@ def is_tech_number(number):
     result = (
         int(str_num[ : half_len]) + int(str_num[half_len : ])
     ) ** 2 == number
- 
-    if length % 2 != 0:
-        return False
-    else:
-        return result
+    
+    return False if length % 2 != 0 else result
     
 def generate_tech_numbers(n):
     numbers = [c for x in range(4, 10 ** 4) if is_tech_number(c := x ** 2)]
-    return numbers[ : 10]
+    return numbers[ : n]
 
 generate_tech_numbers(10)
