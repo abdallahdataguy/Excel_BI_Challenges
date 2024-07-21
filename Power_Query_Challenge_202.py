81ffdfe4adc7913f1ab585a88fb99b7fecc56f00
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-07-21 17:24:55 +0300

    Update Power_Query_Challenge_202.py

--- Power_Query_Challenge_202.py
+++ Power_Query_Challenge_202.py
@@ -20,7 +20,7 @@ df = df.astype(str).replace('0', '')
 df['Level2'] = df['Level2'] * ((df['Name2'] != '') + (df['Name3'] != ''))
 df['Name1'] = name1
 df['Serial'] = df.apply(lambda x: '.'.join([y for y in x[3:] if y]), axis=1)
-df['Names'] = df.apply(lambda x: '.'.join([y for y in x[:3] if y]), axis=1)
+df['Names'] = df.apply(lambda x: ''.join(x[:3]), axis=1)
 df = df[['Serial', 'Names']]
 
 # Display the final results.
