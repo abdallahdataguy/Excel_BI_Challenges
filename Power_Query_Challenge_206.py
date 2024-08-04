870a2e91827106c7f912960bc76f6ffaf1c8a59f
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-08-05 02:38:43 +0300

    Update Power_Query_Challenge_206.py

--- Power_Query_Challenge_206.py
+++ Power_Query_Challenge_206.py
@@ -8,7 +8,6 @@ df = xl("A1:D13", headers=True)
 
 # Perform data munging
 df = df.drop(index=[3, 10]).reset_index(drop=True)
-df[['Value1', 'Value2']] = df[['Value1', 'Value2']].fillna(0).astype(int)
 
 df = pd.concat(
     [
@@ -21,7 +20,7 @@ dfs = [df.iloc[i : i + 6, :].reset_index(drop=True) for i in df.index[::6]]
 dfs = [pd.concat([df.iloc[i : i + 2, :] for i in df.index[::2]], axis=1) for df in dfs]
 for dfi in dfs:
     dfi.columns = ['Column' + str(i + 1) for i in range(len(dfi.columns))]
-df = pd.concat(dfs).replace(0, '').dropna(how='all', ignore_index=True).fillna('')
+df = pd.concat(dfs).dropna(how='all', ignore_index=True).fillna('')
 
 # Display the final results
 df
