fb6e7a67763bd1baf56769111cbd3989eb652dbd
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-08-10 10:00:07 +0300

    Update Power_Query_Challenge_207.py

--- Power_Query_Challenge_207.py
+++ Power_Query_Challenge_207.py
@@ -7,16 +7,16 @@
 df = xl("A2:H13", headers=True)
 
 # Perform data wrangling
-df = df.melt(id_vars='Name', value_vars=df.columns[1:])
-
 values = []
-for day in df['variable'].unique():
-    names = ', '.join(df['Name'][(df['variable'] == day) & (df['value'] == 'Y')])
-    values.append([day, names])
+for col in df.columns[1 : ]:
+    names = ', '.join(df['Name'][df[col] == 'Y'])
+    values.append([col, names])
 
 df = pd.DataFrame(data=values, columns=['Day of Week', 'names'])
 df = pd.concat([df, df['names'].str.split(', ', expand=True)], axis=1)
-df.columns = ['Week ' + str(x + 1) if str(x).isdigit() else x for x in df.columns]
+df.columns = [
+    'Week ' + str(x + 1) if str(x).isdigit() else x for x in df.columns
+]
 df = df.drop(columns='names').fillna('')
 
 # Display the final results
