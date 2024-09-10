da561ce2cf36e785830de0fa868cfa86ecc27679
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-09-10 19:06:51 +0300

    Create Excel_Challenge_540_Right_Angled_Triangles.py

---
+++ Excel_Challenge_540_Right_Angled_Triangles.py
@@ -0,0 +1,27 @@
+# Python in Excel
+
+# Link to the challenge
+# https://www.linkedin.com/posts/excelbi_excel-challenge-problem-activity-7239120428235915264-3zXz/
+
+from itertools import combinations
+
+# Create a function to generate the required results
+def get_perp_base(row):
+    items = row.values
+    for comb in combinations(range(1, items[0]), 2):
+        cond1 = 0.5 * comb[0] * comb[1] == items[1]
+        cond2 = comb[0] ** 2 + comb[1] ** 2 == items[0] ** 2
+        cond3 = comb[0] == int(comb[0])
+        cond4 = comb[1] == int(comb[1])
+        if all([cond1, cond2, cond3, cond4]):
+            return comb
+    return 'NP', 'NP'
+
+# Read the data range
+df = xl("A2:B10", headers=True)
+
+# Perform data manipulation  
+df[['Perpendicular', 'Base']] = df.apply(get_perp_base, axis=1).tolist()
+
+# Display the final results
+df
