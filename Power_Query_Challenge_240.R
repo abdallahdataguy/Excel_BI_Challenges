f00f8ce7037a37bf99b5dfb4a88df2c7623cf5bf
Abdallah Ally <31002385+abdallahdataguy@users.noreply.github.com>
2024-12-08 00:11:56 +0300

    Update Power_Query_Challenge_240.R

--- Power_Query_Challenge_240.R
+++ Power_Query_Challenge_240.R
@@ -3,7 +3,7 @@ library(tidyr)
 library(dplyr)
 
 # Load the Excel file
-file_path <- 'C:/Users/ABDALLAH/Downloads/PQ_Challenge_240.xlsx'
+file_path <- 'PQ_Challenge_240.xlsx'
 df <- read_excel(file_path, range = cell_cols('A:C'))
 
 # Perform data manipulation
